94b2c889159249bd1c1e4dd5f6f9e79c4bf97d1c
aravindr1418 <104976978+aravindr1418@users.noreply.github.com>
2023-07-11 14:02:33 +0530

    Update add.java

--- add.java
+++ add.java
@@ -1,7 +1,7 @@
 public class add{
     public static void main(String[] args){
       System.out.println("hai i am aravind");
-     int a=10;
+     int a=60;
         int b=20;
         System.out.println(a+b);
     }
